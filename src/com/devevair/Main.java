154b386347c9b3db6a0ffb754dd1d4ee89ef4b73
Marco Evair <mmacolluncoc@uni.pe>
2021-06-03 00:47:40 -0500

    write my first println with java 11

---
+++ src/com/devevair/Main.java
@@ -0,0 +1,9 @@
+package com.devevair;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Horra my first java app");
+    }
+}
