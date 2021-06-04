23c1e608f45234a47639f231dff8b27b5cd0307a
Marco Evair <mmacolluncoc@uni.pe>
2021-06-03 22:03:15 -0500

    deep knowloadge of sTRING

--- src/com/devevair/Main.java
+++ src/com/devevair/Main.java
@@ -2,6 +2,7 @@ package com.devevair;
 
 import java.time.LocalDate;
 import java.util.Date;
+import java.util.Locale;
 
 public class Main {
 
@@ -18,8 +19,11 @@ public class Main {
         float theFloat = 3.14F;
 
         String dev = new String("developer");
-        System.out.println(dev);
-
+        System.out.println(dev.toUpperCase());
+        System.out.println(dev.charAt(1));
+        String code = "dev"
+        System.out.println(dev.contains("dev")); // this is a boolean
+        System.out.println(code.equals(dev)); //this is a boolean
         LocalDate now = LocalDate.now();  // you need to import java.time.LocalDate
         System.out.println(now.getMonth());
 
