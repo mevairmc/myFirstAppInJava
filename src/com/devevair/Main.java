1c3c87614f48f08d8132f68643764638e2d8eadf
Marco Evair <mmacolluncoc@uni.pe>
2021-06-03 22:17:01 -0500

    using Math pacakge

--- src/com/devevair/Main.java
+++ src/com/devevair/Main.java
@@ -21,11 +21,26 @@ public class Main {
         String dev = new String("developer");
         System.out.println(dev.toUpperCase());
         System.out.println(dev.charAt(1));
-        String code = "dev"
+        String code = "dev";
         System.out.println(dev.contains("dev")); // this is a boolean
         System.out.println(code.equals(dev)); //this is a boolean
         LocalDate now = LocalDate.now();  // you need to import java.time.LocalDate
         System.out.println(now.getMonth());
 
+
+        // Arithmetic operations
+        System.out.println(10 + 2);
+        System.out.println(10 - 2);
+        System.out.println(10 * 2);
+        System.out.println(10 % 2);
+
+        // the math class
+        System.out.println(Math.abs(-7));
+        System.out.println(Math.max(3.6,10.11));
+        System.out.println(Math.pow(5.0, 2));
+        System.out.println(Math.sqrt(25.0));
+        System.out.println(Math.PI);
+
+        // Comparison Operators
     }
 }
